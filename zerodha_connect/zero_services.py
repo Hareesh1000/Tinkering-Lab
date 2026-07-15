2eb34a5b7ccc3e766b5f11d4bd25e51bb6ec623a
Hareesh <hareeshraj10@gmail.com>
2026-07-15 23:49:46 +0530

    Auto mail enhancement in Zero services

--- zerodha_connect/zero_services.py
+++ zerodha_connect/zero_services.py
@@ -4,8 +4,15 @@ import zero_app_get_data
 ## get the data from the json file and send it to the email
 def send_email_with_portfolio_data():
     data = zero_app_get_data.get_tradingsymbols_from_portfolio_json()
-    send_automail(data)
-
+    print("Portfolio data retrieved from JSON file:")
+    print(data)
+    ## if any one of the stocks has difference_percentage greater than 0 then send the email
+    for stock in data:
+        if stock["difference_percentage"] > 0:
+            print("Sending email with portfolio data...")
+            send_automail(data)
+        else:
+            print("No stocks with positive difference percentage. Email not sent.")
 
 if __name__ == "__main__":
     send_email_with_portfolio_data()
